effd74d5b2f861289f09a7bc11ba1aac39184dbe
nhuanh9 <chopperlouis@gmail.com>
2023-06-21 00:59:06 +0700

    init index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import http from 'http';
+import fs from 'fs';
+import qs from 'qs';
+let count = 0;
+const server = http.createServer((req, res) => {
+});
+server.listen('8080', () => {
+    console.log('Đã kết nối')
+})
