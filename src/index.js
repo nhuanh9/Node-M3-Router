9d3b53c41165ce5130a541a21290e3a3c815acb1
nhuanh9 <chopperlouis@gmail.com>
2023-06-21 01:01:44 +0700

    done config router index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import http from 'http';
 import fs from 'fs';
 import qs from 'qs';
+import router from "./router/router.js";
+
 let count = 0;
 const server = http.createServer((req, res) => {
+    let handle = router[req.url];
+    if (handle === undefined) {
+        handle = router['/err'];
+    }
+    handle(req, res);
 });
 server.listen('8080', () => {
     console.log('Đã kết nối')
---
+++ src/router/router.js
@@ -0,0 +1,4 @@
+let router = {
+
+}
+export default router;
